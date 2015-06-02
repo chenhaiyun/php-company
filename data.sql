819aefb24a726abb894c75e4e885724ffccfc42c
magicchen <282358386@qq.com>
2015-06-02 16:50:00 +0800

    Backend With SBAdmin

--- data.sql
+++ data.sql
@@ -1,12 +1,12 @@
-CREATE DATABASE 'php_company' /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
+CREATE DATABASE php_company /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
 
-CREATE TABLE 'com_admin' (
-  'id' int(11) NOT NULL AUTO_INCREMENT,
-  'login_name' varchar(45) COLLATE utf8_bin NOT NULL,
-  'password' varchar(45) COLLATE utf8_bin NOT NULL,
-  'user_name' varchar(45) COLLATE utf8_bin NOT NULL,
-  'grade' varchar(45) COLLATE utf8_bin NOT NULL,
-  PRIMARY KEY ('id')
+CREATE TABLE com_admin (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  login_name varchar(45) COLLATE utf8_bin NOT NULL,
+  password varchar(45) COLLATE utf8_bin NOT NULL,
+  user_name varchar(45) COLLATE utf8_bin NOT NULL,
+  grade varchar(45) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 /* 插入管理员数据 */
@@ -24,43 +24,43 @@ VALUES
 1);
 
 
-CREATE TABLE 'com_category' (
-  'id' int(11) NOT NULL AUTO_INCREMENT,
-  'category_name' varchar(45) COLLATE utf8_bin NOT NULL,
-  'parent_id' int(11) NOT NULL,
-  'content' varchar(45) COLLATE utf8_bin DEFAULT NULL,
-  'rank' int(11) NOT NULL,
-  'path' varchar(45) COLLATE utf8_bin DEFAULT NULL,
-  PRIMARY KEY ('id')
+CREATE TABLE com_category (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  category_name varchar(45) COLLATE utf8_bin NOT NULL,
+  parent_id int(11) NOT NULL,
+  content varchar(45) COLLATE utf8_bin DEFAULT NULL,
+  rank int(11) NOT NULL,
+  path varchar(45) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=60 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 
-CREATE TABLE 'com_news' (
-  'id' int(11) NOT NULL AUTO_INCREMENT,
-  'news_title' varchar(45) COLLATE utf8_bin NOT NULL,
-  'news_content' varchar(45) COLLATE utf8_bin NOT NULL,
-  'news_category' varchar(45) COLLATE utf8_bin NOT NULL,
-  'news_poster' varchar(45) COLLATE utf8_bin DEFAULT NULL,
-  'news_date' datetime NOT NULL,
-  PRIMARY KEY ('id')
+CREATE TABLE com_news (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  news_title varchar(45) COLLATE utf8_bin NOT NULL,
+  news_content varchar(45) COLLATE utf8_bin NOT NULL,
+  news_category varchar(45) COLLATE utf8_bin NOT NULL,
+  news_poster varchar(45) COLLATE utf8_bin DEFAULT NULL,
+  news_date datetime NOT NULL,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 
-CREATE TABLE 'com_news_category' (
-  'id' int(11) NOT NULL AUTO_INCREMENT,
-  'category_name' varchar(45) COLLATE utf8_bin NOT NULL,
-  'category_desc' varchar(45) COLLATE utf8_bin NOT NULL,
-  PRIMARY KEY ('id')
+CREATE TABLE com_news_category (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  category_name varchar(45) COLLATE utf8_bin NOT NULL,
+  category_desc varchar(45) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 
-CREATE TABLE 'com_product' (
-  'id' int(11) NOT NULL AUTO_INCREMENT,
-  'product_category' varchar(45) COLLATE utf8_bin NOT NULL,
-  'product_name' varchar(45) COLLATE utf8_bin NOT NULL,
-  'product_image' varchar(45) COLLATE utf8_bin DEFAULT NULL,
-  'product_desc' varchar(45) COLLATE utf8_bin DEFAULT NULL,
-  PRIMARY KEY ('id')
+CREATE TABLE com_product (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  product_category varchar(45) COLLATE utf8_bin NOT NULL,
+  product_name varchar(45) COLLATE utf8_bin NOT NULL,
+  product_image varchar(45) COLLATE utf8_bin DEFAULT NULL,
+  product_desc varchar(45) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 
